795294aeccf3a1909e8babacfa2bff46b8673fb0
olha.codes <57961694+olhacodes@users.noreply.github.com>
2022-10-25 13:24:10 +0300

    started project, added new method to the main component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-list';
+  title = 'todo-list'
+  tasks = [
+    "Visit Ann",
+    "Call Dad",
+    "Go to the gym",
+    "Wash the dishes",
+    "Shop for the party"
+  ]
+  add(newTask: string) {
+    alert(newTask)
+  }
 }
+
